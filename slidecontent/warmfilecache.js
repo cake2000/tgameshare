2d87f4a9b35c38e25a327f2632ade6c00b06100f
binyu <bin.yu.private@gmail.com>
2020-07-19 22:06:46 -0400

    f

---
+++ slidecontent/warmfilecache.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+const { execSync } = require('child_process');
+
+function warm() {
+    var fc = fs.readFileSync("filelist", 'utf8'); //Assets.getText(`NewCourses/algo/${f.filename}`);
+    var files = fc.split("\n");
+    for (let i=0; i<files.length; i++) {
+        const fn = files[i];
+        if (fn.length < 4) continue;
+        if (fn.includes("/node_modules/")) continue;
+        let cmd = "wget -O tmp https://cdn.jsdelivr.net/gh/cake2000/tgameshare/slidecontent/" + fn.substring(2);
+        console.log(i + ": " + cmd);
+        try {
+            execSync(cmd);
+        } catch (e) {}
+        break;
+    }
+};
+
+
+warm();
