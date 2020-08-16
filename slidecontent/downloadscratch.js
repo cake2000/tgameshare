ac6e8bcd22aa86b4dd5d10eee80e3b94856c3b6f
binyu <bin.yu.private@gmail.com>
2020-08-16 14:56:26 -0400

    apple

--- slidecontent/downloadscratch.js
+++ slidecontent/downloadscratch.js
@@ -163,7 +163,7 @@ async function init() {
         let fn = files[i];
         // console.log("\n\n\n" + fn);
         console.log("next file " + i + " " + fn + " " + fn.indexOf("flappybird"));
-        if (fn.indexOf("candy") < 0) {
+        if (fn.indexOf("apple") < 0) {
             continue;
         }
         if (fn.indexOf("-") > 0) {
